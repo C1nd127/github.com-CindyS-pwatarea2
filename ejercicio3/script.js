2da65ae198f6ede40628db5958cdfa9b779f607d
Cini <149028949+C1nd127@users.noreply.github.com>
2023-11-07 23:05:05 -0500

    Add files via upload

---
+++ ejercicio3/script.js
@@ -0,0 +1,7 @@
+
+function cambiarImagen() {
+  document.getElementById('imagenCambiante').src = 'img/aurora.jpg';
+}
+function restaurarImagen() {
+  document.getElementById('imagenCambiante').src = 'img/globo.jpg';
+}
